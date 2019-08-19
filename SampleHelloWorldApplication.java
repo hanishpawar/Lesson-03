2494f83739cb61f5063b832947526fd141d47014
Ubuntu <ubuntu@ip-172-31-2-197.ec2.internal>
2019-08-19 15:39:02 +0000

    Added a sample Java file for Hello World

---
+++ SampleHelloWorldApplication.java
@@ -0,0 +1,11 @@
+/**
+ * This is a sample HelloWorldApplication class which implements and prints "Hello World" to the standard output
+ */
+
+public class SampleHelloWorldApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World !!!!!");
+	}
+
+}
